16767aee4509589f4f48217743d7cbbcaf55167b
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2025-10-24 22:07:26 +0530

    grid line

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,9 @@ import { Button, buttonVariants } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      Hi I am Ashwin 
-      <br/>
-      <Button>Hello</Button>
+       <div className="grid-background"></div>
+          
+       
     </div>
   )
 }
