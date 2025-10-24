ac90f728dcb0d71d797fc4d4275c745241c6eb0d
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2025-10-24 17:51:31 +0530

    clerk Installation

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,8 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           {/*Header */}
+          <Header />
+
           <main className="min-h-screen">
            {children}
           </main>
