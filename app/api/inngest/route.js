d298671512f72d1d56dfaca955701dd9b847574d
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2025-11-23 05:48:34 +0530

    inngest complete

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,25 @@
 import { serve } from "inngest/next";
 
-import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/function";
+import { inngest } from "@/lib/inngest/client.js";
+import { generateIndustryInsights } from "@/lib/inngest/function.js";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [generateIndustryInsights],
+  functions: [
+    generateIndustryInsights
+  ],
 });
+// import { serve } from "inngest/next";
+
+// import { inngest } from "@/lib/inngest/client.js";
+// // import { generateIndustryInsights, helloWorld } from "@/lib/inngest/function.js";
+// import { generateIndustryInsights } from "@/lib/inngest/function.js";
+
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [
+//     // helloWorld,
+//     generateIndustryInsights
+//     /* your functions will be passed here later! */
+//   ],
+// });
